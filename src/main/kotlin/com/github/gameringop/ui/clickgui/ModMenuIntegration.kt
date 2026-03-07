f77dbbf9c6abb7ee43086a2b44467ea8b9c43334
TheGameringOP <46537913+TheGameringOP@users.noreply.github.com>
2026-03-07 18:32:05 +0200

    Update ModMenuIntegration.kt

--- src/main/kotlin/com/github/gameringop/ui/clickgui/ModMenuIntegration.kt
+++ src/main/kotlin/com/github/gameringop/ui/clickgui/ModMenuIntegration.kt
@@ -4,5 +4,5 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory
 import com.terraformersmc.modmenu.api.ModMenuApi
 
 class ModMenuIntegration : ModMenuApi {
-    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> = ConfigScreenFactory { ClickGUIScreen }
+    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> = ConfigScreenFactory { ClickGuiScreen }
 }
